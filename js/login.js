0fd3edef0bf8be1d1b21e2079e79d8f59afbe287
Alexandre Henrique <galeriaeg@gmail.com>
2021-03-13 00:52:47 -0300

    novo envio do JS
    terceiro envio do JS

--- js/login.js
+++ js/login.js
@@ -13,3 +13,29 @@ function mostraSenha(a){
 		document.getElementById('inputSenha').type="password";
 	}
 }
+
+
+
+
+function validaLogin(formLogin){
+	
+	if (formLogin.login.value == ""){ 
+		//alert("Informe seu nome!");
+		document.getElementById('alerta').style.display="block";
+		document.getElementById('alerta').innerHTML="Informe o seu login!";
+		formLogin.login.focus();
+		return (false); 
+	}
+	if (formLogin.senha.value == ""){ 
+		document.getElementById('alerta').style.display="block";
+		document.getElementById('alerta').innerHTML="Informe a sua senha!";
+		formLogin.senha.focus();
+		return (false); 
+	}
+
+}
+
+function limpaAlerta(){
+	document.getElementById('alerta').style.display="none";
+	document.getElementById('alerta').innerHTML="";	
+}
